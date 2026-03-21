30559fd352813e0162ebc5a54f4f5f4148bd9293
naitokosuke <kosuke.naito.engineer@gmail.com>
2026-03-21 18:31:29 +0900

    feat: impl peek

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,16 @@ enum JsonValue {
     Array(Vec<JsonValue>),
     Object(Vec<(String, JsonValue)>),
 }
+
+struct Parser {
+    input: Vec<char>, // input string as a vector of characters
+    pos: usize,       // current position in the input
+}
+
+impl Parser {
+    /// Returns the character at the current position without advancing.
+    /// Returns `None` if the position is past the end of input.
+    fn peek(&self) -> Option<char> {
+        self.input.get(self.pos).copied()
+    }
+}
