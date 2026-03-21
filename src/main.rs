7a964926e468cb765067f8be9a9aa0032a2027e8
naitokosuke <kosuke.naito.engineer@gmail.com>
2026-03-21 18:12:41 +0900

    chore

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     Ok(())
 }
+
+enum JsonValue {
+    Null,
+    Bool(bool),
+    Number(f64),
+    String(String),
+    Array(Vec<JsonValue>),
+    Object(Vec<(String, JsonValue)>),
+}
