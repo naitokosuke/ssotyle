beca43f20920110eca75f00faa49c25064c7beed
naitokosuke <kosuke.naito.engineer@gmail.com>
2026-03-21 12:19:08 +0900

    feat

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    println!("{:?}", args);
+    match args.as_slice() {
+        [_, filepath] => {
+            println!("{}", filepath);
+        }
+        _ => {
+            eprintln!("Usage: ssotyle <file>");
+            std::process::exit(1);
+        }
+    }
 }
